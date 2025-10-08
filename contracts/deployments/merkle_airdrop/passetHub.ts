c77c21115b5488a42abc7195ac4fde13ebd64038
truthixify <truthixify@gmail.com>
2025-10-08 01:29:09 +0100

    made some new changes

--- contracts/deployments/merkle_airdrop/passetHub.ts
+++ contracts/deployments/merkle_airdrop/passetHub.ts
@@ -1,4 +1,4 @@
-export const ss58Address = "5CgdHUBvEdQyBLDH1XczfbDpKTbDTCRc4xyu1XEoZbSmoASd"
-export const evmAddress = "0x1b657146e786ec4c098508ebb3dcaf13d0ea84af"
-export const blockHash = "0x3876ce02b0931c9e6fdd68a7549b09b22bf7d53de268478c325f66533aa76e02"
-export const blockNumber = 1471130
+export const ss58Address = "5GNwzU1G1vkb5bBQDHbHdqatmrSvueFYK2zDKTCGLpknJoC7"
+export const evmAddress = "0xbedaee8eb3649c44b93e63065b66b087935269ee"
+export const blockHash = "0x559a60dd2618a2e21e1787c076e4b7b8ffe33ff59bd62f15dea2db5dc3fe153e"
+export const blockNumber = 1488966
